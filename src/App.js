5c42f182f02f3218c81e722e30ee3f6434a6eac4
Ajinkya-Dhavale <dhavaleajinkya9604@gmail.com>
2025-02-27 12:22:55 +0530

    chnages in app.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>hello everyone</p>
-        <p>thank</p>
         <a
           className="App-link"
           href="https://reactjs.org"
